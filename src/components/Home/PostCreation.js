da08cff85cf98f82098ee52bbd5b2a976ee64db8
Subrat29 <ankubanku29062003@gmail.com>
2024-05-29 22:49:58 +0530

    succesfully integrate update post api to frontend

--- src/components/Home/PostCreation.js
+++ src/components/Home/PostCreation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PostCreateModel from './Post/PostCreateModel';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPost, updatePost } from '../../feature/postSlice';
@@ -11,17 +11,25 @@ const PostCreation = ({ post }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [image, setImage] = useState(null);
+  const [imageChanged, setImageChanged] = useState(false);
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const userData = useSelector((state) => state.auth.userData)
-  const profileData = useSelector((state) => state.profile.profile)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const profileData = useSelector((state) => state.profile.profile);
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post.Title);
+      setDescription(post.Description);
+      setImage(post.Image);
+    }
+  }, [post]);
 
   function getCurrentDateTime() {
     const now = new Date();
     const date = now.toLocaleDateString();  // Format: MM/DD/YYYY
     const time = now.toLocaleTimeString();  // Format: HH:MM:SS AM/PM
-
     return `${date} ${time}`;
   }
 
@@ -34,12 +42,12 @@ const PostCreation = ({ post }) => {
     setTitle('');
     setDescription('');
     setImage(null);
+    setImageChanged(false);
   };
 
   const handleUpload = async () => {
-    if (!image) {
-      console.log('No file selected or file type is not an image');
-      return null;
+    if (!imageChanged) {
+      return image;
     }
 
     const formData = new FormData();
@@ -49,7 +57,6 @@ const PostCreation = ({ post }) => {
 
     try {
       const response = await axios.post('https://api.cloudinary.com/v1_1/dnjis096o/image/upload', formData);
-      console.log("response", response)
       return response.data.secure_url;
     } catch (error) {
       console.error('Error uploading file:', error);
@@ -59,31 +66,41 @@ const PostCreation = ({ post }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const uploadedFile = await handleUpload();
 
     if (post) {
       // Update Post
-      console.log("Update Post")
       const updatedPost = {
-        ...post,
-        updatedAt: getCurrentDateTime(),
-        Title: title || post.Title,
-        Image: image || post.Image,
-        Description: description || post.Description
-      }
-      dispatch(updatePost(updatedPost))
-      navigate(`/post/${updatedPost._id}`)
-    }
-    else {
-      // Create Post
-      console.log("Create Post")
-
-      const uploadedFile = await handleUpload();
-      const newPost = {
-        // Image: "https://res.cloudinary.com/dnjis096o/image/upload/v1716868344/sbunyrnmkh6swgfgzy9l.jpg",
         Image: uploadedFile,
         Title: title,
         Description: description
+      };
+
+      try {
+        const response = await api.put(`post/updatepost/${post._id}`,
+          updatedPost,
+          {
+            headers: {
+              'Authorization': `Bearer ${userData.Token}`,
+            },
+          }
+        );
+
+        alert('Post updated successfully');
+        dispatch(updatePost(response.data.post));
+        navigate(`/post/${response.data.post._id}`);
+      } catch (error) {
+        alert('Error updating post');
+        console.error(error);
       }
+    } else {
+      // Create Post
+      const newPost = {
+        Image: uploadedFile,
+        Title: title,
+        Description: description
+      };
+
       try {
         const response = await api.post(`post/createpost/${profileData._id}`,
           newPost,
@@ -96,9 +113,8 @@ const PostCreation = ({ post }) => {
         );
 
         alert('Post created successfully');
-        console.log(response);
-        dispatch(addPost(response.data.post))
-        navigate(`/post/${response.data.post._id}`)
+        dispatch(addPost(response.data.post));
+        navigate(`/post/${response.data.post._id}`);
       } catch (error) {
         alert('Error creating post');
         console.error(error);
@@ -109,12 +125,12 @@ const PostCreation = ({ post }) => {
 
   if (post) return (
     <form onSubmit={handleSubmit}>
-      <h2 className=" flex justify-center text-2xl text-white mb-4">Edit Post</h2>
+      <h2 className="text-2xl text-white mb-4 flex justify-center">{post ? 'Edit Post' : 'Create a Post'}</h2>
       <div className="mb-4">
         <label className="block text-sm text-gray-400 mb-1">Title</label>
         <input
           type="text"
-          value={title || post.Title}
+          value={title}
           onChange={(e) => setTitle(e.target.value)}
           className="w-full p-2 rounded bg-gray-600 border border-gray-500"
           required
@@ -123,7 +139,7 @@ const PostCreation = ({ post }) => {
       <div className="mb-4">
         <label className="block text-sm text-gray-400 mb-1">Description</label>
         <textarea
-          value={description || post.Description}
+          value={description}
           onChange={(e) => setDescription(e.target.value)}
           className="w-full p-2 rounded bg-gray-600 border border-gray-500"
           rows="4"
@@ -134,7 +150,11 @@ const PostCreation = ({ post }) => {
         <label className="block text-sm text-gray-400 mb-1">Image Upload</label>
         <input
           type="file"
-          onChange={(e) => setImage(e.target.files[0])}
+          accept='image/*'
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+            setImageChanged(true);
+          }}
           className="w-full p-2 rounded bg-gray-600 border border-gray-500"
         />
       </div>
@@ -142,7 +162,7 @@ const PostCreation = ({ post }) => {
         type="submit"
         className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded-lg"
       >
-        Post
+        {post ? 'Update Post' : 'Create Post'}
       </button>
     </form>
   )
@@ -164,7 +184,7 @@ const PostCreation = ({ post }) => {
 
       <PostCreateModel isOpen={isModalOpen} onClose={handleCloseModal}>
         <form onSubmit={handleSubmit}>
-          <h2 className="text-2xl text-white mb-4">Create a Post</h2>
+          <h2 className="text-2xl text-white mb-4">{post ? 'Edit Post' : 'Create a Post'}</h2>
           <div className="mb-4">
             <label className="block text-sm text-gray-400 mb-1">Title</label>
             <input
@@ -190,7 +210,10 @@ const PostCreation = ({ post }) => {
             <input
               type="file"
               accept='image/*'
-              onChange={(e) => setImage(e.target.files[0])}
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+                setImageChanged(true);
+              }}
               className="w-full p-2 rounded bg-gray-600 border border-gray-500"
             />
           </div>
@@ -198,7 +221,7 @@ const PostCreation = ({ post }) => {
             type="submit"
             className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded-lg"
           >
-            Post
+            {post ? 'Update Post' : 'Create Post'}
           </button>
         </form>
       </PostCreateModel>
